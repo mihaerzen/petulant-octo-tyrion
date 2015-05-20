f969498563dab039be7bba3189ccf8b73402b225
Miha Eržen <miha.erzen1@gmail.com>
2015-05-20 12:17:50 +0200

    Test passing

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+var should = require('chai').should();
+
+describe('Test', function () {
+
+    it('Should pass', function (done) {
+        assert.equal(0, 0);
+        done();
+    });
+});
